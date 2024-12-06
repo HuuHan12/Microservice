c61ce59952d5b1e0bc079d6bebd11414e62aef48
HuuHan12 <dnhan.a7.c3tqcap@gmail.com>
2024-12-06 17:17:34 +0700

    Add application.properties

--- bookservice/src/main/java/com/ltfullstack/bookservice/BookserviceApplication.java
+++ bookservice/src/main/java/com/ltfullstack/bookservice/BookserviceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class BookserviceApplication {
+	public class BookserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookserviceApplication.class, args);
