7652ee1ee22c51829c1c7dce5197debfe522e5ab
HuuHan12 <dnhan.a7.c3tqcap@gmail.com>
2025-05-01 22:52:02 +0700

    h2 database

---
+++ bookservice/src/main/java/com/ltfullstack/bookservice/command/data/Book.java
@@ -0,0 +1,25 @@
+package com.ltfullstack.bookservice.command.data;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String author;
+    private boolean isReady;
+
+
+}
