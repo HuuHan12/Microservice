8cf771f9281bda579e06d183205f82bdf7270c9f
HuuHan12 <dnhan.a7.c3tqcap@gmail.com>
2025-05-12 22:54:45 +0700

    API Add Employee

---
+++ employeeservice/src/main/java/com/ltfullstack/employeeservice/command/data/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.ltfullstack.employeeservice.command.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, String> {
+}
