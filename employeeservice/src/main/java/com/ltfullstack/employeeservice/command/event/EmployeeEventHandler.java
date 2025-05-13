9c5e4557536f9f101c56f1002577ffab7157f51b
HuuHan12 <dnhan.a7.c3tqcap@gmail.com>
2025-05-13 14:38:24 +0700

    CRUD for Employee

--- employeeservice/src/main/java/com/ltfullstack/employeeservice/command/event/EmployeeEventHandler.java
+++ employeeservice/src/main/java/com/ltfullstack/employeeservice/command/event/EmployeeEventHandler.java
@@ -2,11 +2,17 @@ package com.ltfullstack.employeeservice.command.event;
 
 import com.ltfullstack.employeeservice.command.data.Employee;
 import com.ltfullstack.employeeservice.command.data.EmployeeRepository;
+import jakarta.ws.rs.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.eventhandling.DisallowReplay;
 import org.axonframework.eventhandling.EventHandler;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
+@Slf4j
 @Component
 public class EmployeeEventHandler {
 
@@ -14,9 +20,33 @@ public class EmployeeEventHandler {
     private EmployeeRepository employeeRepository;
 
     @EventHandler
-    public void on (EmployeeCreatedEvent event){
+    public void on(EmployeeCreatedEvent event) {
         Employee employee = new Employee();
         BeanUtils.copyProperties(event, employee);
         employeeRepository.save(employee);
+    }
+
+    @EventHandler
+    public void on(EmployeeUpdatedEvent event) throws Exception {
+        Optional<Employee> oldEmployee = employeeRepository.findById(event.getId());
+        Employee employee = oldEmployee.orElseThrow(() -> new Exception("Employee not found"));
+
+        employee.setFirstName(event.getFirstName());
+        employee.setLastName(event.getLastName());
+        employee.setKin(event.getKin());
+        employee.setIsDisciplined(event.getIsDisciplined());
+        employeeRepository.save(employee);
+    }
+
+    @EventHandler
+    @DisallowReplay
+    public void on(EmployeeDeletedEvent event){
+        try {
+            employeeRepository.findById(event.getId()).orElseThrow(() -> new Exception("Employee not found"));
+            employeeRepository.deleteById(event.getId());
+        } catch (Exception ex) {
+            log.error(ex.getMessage());
+        }
+
     }
 }
