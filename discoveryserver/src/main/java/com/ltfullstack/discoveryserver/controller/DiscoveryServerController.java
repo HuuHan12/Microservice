23765b8cd7d4f8a43df541833b26a882e5a7bdb4
HuuHan12 <dnhan.a7.c3tqcap@gmail.com>
2025-06-02 00:18:05 +0700

    Update Workflow URL

--- discoveryserver/src/main/java/com/ltfullstack/discoveryserver/controller/DiscoveryServerController.java
+++ discoveryserver/src/main/java/com/ltfullstack/discoveryserver/controller/DiscoveryServerController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-@RequestMapping("/api/v1/discovery")
+@RequestMapping("api/v1/discovery")
 public class DiscoveryServerController {
     @GetMapping()
-    public String hello() {
+    public String hello(){
         return "Hello Young";
     }
 }
