6181e3505e68a2957a89c45752fa0df094012f58
HuuHan12 <dnhan.a7.c3tqcap@gmail.com>
2025-06-02 00:06:17 +0700

    Update Workflows

---
+++ discoveryserver/src/main/java/com/ltfullstack/discoveryserver/controller/DiscoveryServerController.java
@@ -0,0 +1,14 @@
+package com.ltfullstack.discoveryserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/api/v1/discovery")
+public class DiscoveryServerController {
+    @GetMapping()
+    public String hello() {
+        return "Hello Young";
+    }
+}
